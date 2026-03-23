b9023c58bc597cb9bb5a82ca9792c544a19825b7
laozishibeigua <2386120070@qq.com>
2026-03-23 11:26:59 +0800

    feat: 支持查询atcoder比赛信息

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from astrbot.api.star import Context, Star
 from astrbot.api import logger
 import httpx
 import time
+from bs4 import BeautifulSoup
 
 class MyPlugin(Star):
     def __init__(self, context: Context):
@@ -104,8 +105,18 @@ class MyPlugin(Star):
         if "男朋友" in message_text or ("男生" in message_text and "可爱" in message_text):
             await event.send(event.plain_result("是男生就不要找男朋友，yx除外"))
     
+    def _build_info_string(self, contests_info, type: str) -> str :
+        final_contest_info = ""
 
-    def get_cf_contest_info(self):
+        for contest_info in contests_info:
+            final_contest_info += "--------------------\n"
+            final_contest_info += contest_info[0] + "\n"
+            final_contest_info += "开始时间：" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(contest_info[1])) + "\n"
+            final_contest_info += "持续时间：" + str(contest_info[2] // 3600) + "小时" + (str(contest_info[2] % 3600 // 60) + "分钟" if contest_info[2] % 3600 != 0 else "") + "\n"
+        
+        return final_contest_info
+
+    def _get_cf_contest_info(self):
         cf_contest_request = httpx.get("https://codeforces.com/api/contest.list?gym=false")
         
         if cf_contest_request.status_code != 200:
@@ -128,26 +139,56 @@ class MyPlugin(Star):
             contest_info.append(contest_result["durationSeconds"])
             contests_info.append(contest_info)
 
-        final_cf_contest_info = ""
-
-        for contest_info in contests_info:
-            final_cf_contest_info +=  "--------------------\n"
-            final_cf_contest_info += contest_info[0] + "\n"
-            final_cf_contest_info += "开始时间：" + time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(contest_info[1])) + "\n"
-            final_cf_contest_info += "持续时间：" + str(contest_info[2] // 3600) + "小时" + (str(contest_info[2] % 3600 // 60) + "分钟" if contest_info[2] % 3600 != 0 else "") + "\n"
+        final_cf_contest_info = self._build_info_string(contests_info, "cf") if contests_info else "好像木有比赛唉\n"
+        
         return final_cf_contest_info
 
-    def get_atc_contest_info(self):
-        pass 
+    def _get_atc_contest_info(self):
+        atc_contest_request = httpx.get("https://atcoder.jp/home")
+
+        if atc_contest_request.status_code != 200:
+            return "小北瓜查不到欸，是不是atc又爆炸了？"
+        
+        elems = BeautifulSoup(atc_contest_request.text,"html.parser").select("#contest-table-upcoming a")
+
+        contest_set = []
+        contests_info = []
+
+        for index, element in enumerate(elems):
+            element_text = str(element.getText())
+            contest_set.append(element_text)
+
+            if index % 2 == 1:
+                element_link = str(element)
+                start_index = element_link.find('"') + 1
+                end_index = element_link[start_index:].find('"') + start_index
+                contest_set.append("https://atcoder.jp" + element_link[start_index : end_index])
+                contest_set[0] = contest_set[0][:-5]
+                contest_set[0], contest_set[1] = contest_set[1], contest_set[0]
+                if "Beginner" in contest_set[0]:
+                    contest_set[2] = 6000
+                if "Regular" in contest_set[0]:
+                    contest_set[2] = 7200
+
+                if "Beginner" in contest_set[0] or "Regular" in contest_set[0]:
+                    contests_info.append(contest_set)
+                    contest_set = []
+        
+        final_atc_contest_info = self._build_info_string(contests_info, "atc") if contests_info else "好像木有比赛唉\n"
+        
+        return final_atc_contest_info
     
     @filter.command("cpcquery")
     async def cpc_query(self, event: AstrMessageEvent):
 
-        cf_context_info = self.get_cf_contest_info()
+        cf_context_info = self._get_cf_contest_info()
+        atc_context_info = self._get_atc_contest_info()
         
         final_message =  "即将到来的比赛：\n"
         final_message += "CodeForces:\n"
         final_message += cf_context_info
+        final_message += "Atcoder:\n"
+        final_message += atc_context_info
 
         await event.send(event.plain_result(final_message))
 
